ce7bea0dd8afc0d7105910161a201859b6e3c180
ItsReyB <37261129+ItsReyB@users.noreply.github.com>
2018-05-25 00:11:53 -0500

    Bootstrap
    Bootstrap

--- ComicCity/resources/views/template/template.blade.php
+++ ComicCity/resources/views/template/template.blade.php
@@ -23,7 +23,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+		<link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<title>@yield('title')-Comic City</title>
